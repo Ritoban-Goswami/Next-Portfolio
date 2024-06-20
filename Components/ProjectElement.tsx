584041e4895ae5e67babca14267dbfc3062656dc
Ritoban Goswami <dev.ritoban.goswami@gmail.com>
2024-06-20 22:35:02 +0530

    feat: added useWindowSize hook to fix project section ui bug

--- Components/ProjectElement.tsx
+++ Components/ProjectElement.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { motion } from "framer-motion";
+import { useWindowSize } from "@/hooks/useWindowSize";
 
 type Props = {
   projectTitle: string;
@@ -26,6 +27,11 @@ const ProjectElement = ({
     show: { opacity: 1 },
   };
 
+  // Determine if the current screen size is mobile
+  const size = useWindowSize();
+  const isMobile = size.width !== undefined && size.width < 768;
+  console.log(isMobile);
+
   return (
     <Link href={`/projects/${encodeURIComponent(projectSlug)}`}>
       <motion.div
@@ -49,10 +55,14 @@ const ProjectElement = ({
           height={1000}
         />
         <div
-          className={`my-6 md:my-0 md:absolute md:inset-0 rounded-md flex flex-col justify-end p-1 md:p-4 lg:opacity-0 md:hover:opacity-100 md:backdrop-blur-sm transition-opacity`}
-          style={{
-            backgroundImage: `linear-gradient(to top, #000000, ${domColor})`,
-          }}
+          className={`project-card my-6 md:my-0 md:absolute md:inset-0 rounded-md flex flex-col justify-end p-1 md:p-4 lg:opacity-0 md:hover:opacity-100 md:backdrop-blur-sm transition-opacity`}
+          style={
+            isMobile
+              ? {}
+              : {
+                  backgroundImage: `linear-gradient(to top, #000000, ${domColor})`,
+                }
+          }
         >
           <h4 className="text-xl text-neutral-100 font-bold mb-4">
             {projectTitle}
